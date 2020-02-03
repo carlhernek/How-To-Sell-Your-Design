973563b791d0a817d9102c8059b8bbe1bbfcd9ef
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-02-03 19:13:26 +0100

    Update index.js

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -6,22 +6,70 @@ export default function Container(props) {
   const [wordOne, setWordOne] = useState();
   const [wordTwo, setWordTwo] = useState();
 
-  const wordArrayOne = ["Timeless", "Minimalistic", "Artful"];
-  const wordArrayTwo = ["Design", "Expression", "Form"];
+  const wordArrayOne = [
+    "Holistic",
+    "Advanced",
+    "Approachable",
+    "Cutting-Edge",
+    "Sophisticated",
+    "Modern",
+    "Timeless",
+    "Tasteful",
+    "Minimalistic",
+    "Characteristic",
+    "Coherent"
+  ];
+  const wordArrayTwo = [
+    "Appearance",
+    "Expression",
+    "Design",
+    "Interior",
+    "Form",
+    "Shape",
+    "Composition",
+    "Layout",
+    "Exterior",
+    "Looks",
+    "Style"
+  ];
 
-  const rnd = function() {
-    return Math.floor(Math.random() * 3);
+  const rnd = () => {
+    return Math.floor(Math.random() * 10);
   };
-  const genWord = param => {
-    setWordOne(wordArrayOne[rnd()]);
-    setWordTwo(wordArrayTwo[rnd()]);
+
+  const genWord = (x, y) => {
+    if (x) {
+      const one = wordArrayOne[rnd()];
+      if (one !== wordOne) {
+        setWord(1, one);
+      } else {
+        genWord(true, false);
+      }
+    }
+    if (y) {
+      const two = wordArrayTwo[rnd()];
+      if (two !== wordTwo) {
+        setWord(2, two);
+      } else {
+        genWord(false, true);
+      }
+    }
+  };
+
+  const setWord = (selector, word) => {
+    if (selector === 1) {
+      setWordOne(word);
+    }
+    if (selector === 2) {
+      setWordTwo(word);
+    }
   };
 
   return (
     <div className="container">
       <Words props={wordOne} />
       <Words props={wordTwo} />
-      <button onClick={() => genWord()}> button</button>
+      <button onClick={() => genWord(true, true)}> button</button>
     </div>
   );
 }
